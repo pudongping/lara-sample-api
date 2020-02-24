7fe17e0717ef9ec119b88ee082e556e98a79f9d5
Alex <1414818093@qq.com>
2020-02-24 21:51:22 +0800

    开启跨域

--- app/Http/Middleware/EnableCross.php
+++ app/Http/Middleware/EnableCross.php
@@ -32,26 +32,5 @@ class EnableCross
         $response->header('Access-Control-Allow-Credentials', 'false');
 
         return $response;
-
-
-        // 前端请求的源 url
-        $origin = $request->server('HTTP_ORIGIN') ? $request->server('HTTP_ORIGIN') : '';
-        dd(4444, $origin);
-        // 允许跨域的 url 列表
-        $allowOrigin = [
-            'http://localhost:8080',
-            'http://localhost:*',
-            'http://shop.drling.xin',
-        ];
-
-        if (in_array($origin, $allowOrigin)) {
-            $response->header('Access-Control-Allow-Origin', $origin);
-            $response->header('Access-Control-Allow-Headers', 'Origin, Content-Type, Cookie, X-CSRF-TOKEN, Accept, Authorization, X-XSRF-TOKEN');
-            $response->header('Access-Control-Expose-Headers', 'Authorization, authenticated');
-            $response->header('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, OPTIONS');
-            $response->header('Access-Control-Allow-Credentials', 'true');
-        }
-
-        return $response;
     }
 }
