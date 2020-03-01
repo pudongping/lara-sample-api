bf5bb58ff5185d02de94a8868410d5c3fe74f6df
Alex <1414818093@qq.com>
2020-03-01 20:35:58 +0800

    Update EnableCross.php

--- app/Http/Middleware/EnableCross.php
+++ app/Http/Middleware/EnableCross.php
@@ -28,7 +28,7 @@ class EnableCross
         $response->header('Access-Control-Allow-Origin', '*');
         $response->header('Access-Control-Allow-Headers', 'Origin, Content-Type, Cookie, X-CSRF-TOKEN, Accept, Authorization, X-XSRF-TOKEN, token');
         $response->header('Access-Control-Expose-Headers', 'Authorization, authenticated');
-        $response->header('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, OPTIONS');
+        $response->header('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE, OPTIONS');
         $response->header('Access-Control-Allow-Credentials', 'false');
 
         return $response;
